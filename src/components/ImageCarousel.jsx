e4c9c3f1c04ec9fe0f66b5846e0e8a29caa06532
Иван Хурбатов <hurbatushka@gmail.com>
2024-06-29 11:21:23 +0300

    upd

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import 'swiper/css/pagination';
 import { useState } from 'react';
 import Image from 'next/image';
 
